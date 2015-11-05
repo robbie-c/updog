cc559ec495a96cb612e8085c3bd01aba4338ac90
Robbie Coomber <robbie.coomber@gmail.com>
2015-11-05 13:47:46 +0000

    Change URLs format

--- src/server/signalling.js
+++ src/server/signalling.js
@@ -108,14 +108,14 @@ function setUpSignalling(server) {
             //stunServers: [{"url": "stun:stun.l.google.com:19302"}],
             //turnServers: [],
             iceServers: [
-                {url: 'stun:stun.l.google.com:19302'},
-                {url: 'stun:stun.services.mozilla.com'},
-                {url: 'stun:stun1.l.google.com:19302'},
-                {url: 'stun:stun2.l.google.com:19302'},
-                {url: 'stun:stun3.l.google.com:19302'},
-                {url: 'stun:stun4.l.google.com:19302'},
+                {urls: 'stun:stun.l.google.com:19302'},
+                {urls: 'stun:stun.services.mozilla.com'},
+                {urls: 'stun:stun1.l.google.com:19302'},
+                {urls: 'stun:stun2.l.google.com:19302'},
+                {urls: 'stun:stun3.l.google.com:19302'},
+                {urls: 'stun:stun4.l.google.com:19302'},
                 {
-                    url: 'turn:numb.viagenie.ca',
+                    urls: 'turn:numb.viagenie.ca',
                     credential: 'updogftw',
                     username: 'robbie.coomber@gmail.com'
                 }
