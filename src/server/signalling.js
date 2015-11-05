226b8ea5b6a6ac0476503a4d8d3a0dfce5730727
Robbie Coomber <robbie.coomber@gmail.com>
2015-11-05 00:06:26 +0000

    Add a free TURN server with demo credentials

--- src/server/signalling.js
+++ src/server/signalling.js
@@ -113,7 +113,12 @@ function setUpSignalling(server) {
                 {url: 'stun:stun1.l.google.com:19302'},
                 {url: 'stun:stun2.l.google.com:19302'},
                 {url: 'stun:stun3.l.google.com:19302'},
-                {url: 'stun:stun4.l.google.com:19302'}
+                {url: 'stun:stun4.l.google.com:19302'},
+                {
+                    url: 'turn:numb.viagenie.ca',
+                    credential: 'updogftw',
+                    username: 'robbie.coomber@gmail.com'
+                }
             ]
         };
 
