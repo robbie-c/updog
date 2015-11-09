7b0d217b1562e41054aee7fdc9be63f49b5f7c0b
Robbie Coomber <robbie.coomber@gmail.com>
2015-11-09 14:34:59 +0000

    Nabber shouldn't be in the container

--- src/components/PermanentCallPage.jsx
+++ src/components/PermanentCallPage.jsx
@@ -7,15 +7,17 @@ var RoomControl = require('./RoomControl.jsx');
 var PermanentCallPage = React.createClass({
     render: function () {
         return (
-            <div className="topLevelContent container">
+            <div className="topLevelContent">
                 <NavBar user={this.props.user} title={this.props.title} room={this.props.room}/>
-                <h1>Hi there!</h1>
-                <div className=" row">
-                    <div className="col-md-8">
-                        <VideoArea user={this.props.user} title={this.props.title} room={this.props.room}/>
-                    </div>
-                    <div className="col-md-4">
-                        <RoomControl user={this.props.user} title={this.props.title} room={this.props.room}/>
+                <div className="container">
+                    <h1>Hi there!</h1>
+                    <div className=" row">
+                        <div className="col-md-8">
+                            <VideoArea user={this.props.user} title={this.props.title} room={this.props.room}/>
+                        </div>
+                        <div className="col-md-4">
+                            <RoomControl user={this.props.user} title={this.props.title} room={this.props.room}/>
+                        </div>
                     </div>
                 </div>
             </div>
