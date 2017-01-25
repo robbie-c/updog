598195cbb52b3dc8b5267c37302c0299f71a64c0
Robbie Coomber <robbie.coomber@gmail.com>
2017-01-25 13:09:17 +0000

    Remove old config stuff as it's no longer accurate
    All those services and accounts have been deactivated, any anyway they should be passed through the ENV rather than checked into git

--- src/config.js
+++ src/config.js
@@ -1,71 +1,12 @@
-var os = require('os');
-var merge = require('deepmerge');
+const os = require('os');
 
-var logger = require('./common/logger');
+export const hostname = os.hostname();
+export const isClient = typeof window !== 'undefined';
+export const isDebug = true; // TODO
+export const iceServers = [
+    {
+        urls: 'stun:stun.l.google.com:19302'
+    }
+];
 
-var hostname = os.hostname();
-
-logger.info('loading config for', hostname);
-
-var config = {
-    sessionSecret: 'dOvgrWZJBeB41k72HAinaFRTRC3fc5ix',
-    tokenSecret: 'PovEvCithek3PovEvCithek3haunEbwib1',
-    postgres: {
-        host: 'ec2-54-217-202-109.eu-west-1.compute.amazonaws.com',
-        port: 5432,
-        database: 'd31jqimr8d6bdl',
-        user: 'uqlgahxmefmvcx',
-        pass: '8MYhGqRLKi053eSASkgfumzM9L'
-    },
-    redis: {
-        host: 'pub-redis-16317.us-east-1-2.5.ec2.garantiadata.com',
-        port: 16317,
-        pass: 'KabiRjTkm2tfCN57Lj3NcAR4jyJby6j7'
-    },
-    mongo: {
-        url: 'mongodb://updog:8IhiLQ0ZycWoYDNyKVelQOwqQ9o39uoy@ds041563.mongolab.com:41563/updog'
-    },
-    googleAuth: {
-        clientId: '640683349524-fhi7nh4ubqe9fogpchb0l6b462psd4ff.apps.googleusercontent.com',
-        clientSecret: 'LONIGF0jmbcZHseZmDC1KZtl',
-        callbackURL: '/auth/google/callback'
-    },
-    iceServers: [
-        /*
-        {
-            urls: 'turn:104.155.75.9:5349',
-            username: 'updog',
-            credential: 'testingPasswordForWIPYp11fY1JSHefiI40FWwFJnUpdXjqDux2'
-        },
-        {
-            urls: 'turn:numb.viagenie.ca',
-            credential: 'updogftw',
-            username: 'robbie.coomber@gmail.com'
-        },
-        */
-        {
-            urls: 'stun:stun.l.google.com:19302'
-        },
-        {
-            urls: 'turn:130.211.59.162',
-            username: 'updog',
-            credential: 'HudpeHev978234kjhsdfkjh'
-        }
-    ]
-};
-
-if (/yellowstone/.test(hostname)) {
-    config = merge(config, {
-        redis: {
-            host: 'pub-redis-16317.us-east-1-2.5.ec2.garantiadata.com',
-            port: 16317,
-            pass: 'KabiRjTkm2tfCN57Lj3NcAR4jyJby6j7'
-        },
-        mongo: {
-            url: 'mongodb://updog:8IhiLQ0ZycWoYDNyKVelQOwqQ9o39uoy@ds041563.mongolab.com:41563/updog'
-        }
-    })
-}
-
-module.exports = config;
 
